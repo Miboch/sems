ff75432c121a9adf1304c970ad6373e5b5078bfc
Mikkel Bo Christensen <mc@airsupport.dk>
2021-09-17 13:49:52 +0200

    sems wip

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
-use std::env;
+use std::str;
+// use std::env;
+use std::{process::Command};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let stdout = Command::new("git")
+    .arg("tag")
+    .output()
+    .unwrap().stdout;
+    let tags = str::from_utf8(stdout.as_slice()).unwrap();
+    // let args: Vec<String> = env::args().collect();
+    println!("{:?}", tags);
 }
