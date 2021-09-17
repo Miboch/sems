0c9dafa9d95c0f019ea0569e7aa7fc5c0debf74b
Mikkel Bo Christensen <mc@airsupport.dk>
2021-09-17 14:04:14 +0200

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 use std::str;
 // use std::env;
-use std::{process::Command};
+use std::process::Command;
 
 fn main() {
-
-    let stdout = Command::new("git")
-    .arg("tag")
-    .output()
-    .unwrap().stdout;
+    let stdout = Command::new("git").arg("tag").output().unwrap().stdout;
     let tags = str::from_utf8(stdout.as_slice()).unwrap();
-    // let args: Vec<String> = env::args().collect();
-    println!("{:?}", tags);
+    let mut tag_vector = tags.split("\n");
+    for s in tag_vector {
+        println!("{:?}", s);
+    }
 }
